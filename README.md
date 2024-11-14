f9baf242d06ce2d452ec044c7c6dba8ec4fa815b
Mateusz Kluba <mateusz.kluba@kalasoft.pl>
2024-11-14 22:35:38 +0100

    refs: - #10 - Homepage manufacturing - readme file

--- README.md
+++
@@ -1,2 +0,0 @@
-# ChelseaPlayersStats
-Web app designed to help managaing and verifying data about football players' statistics presented at https://chelseapoland.com/rozgrywki/statystyki 
---
+++ src/main/java/klubson/chelseapoland/ChelseaPlayersStats/controller/MainPageController.java
@@ -0,0 +1,15 @@
+package klubson.chelseapoland.ChelseaPlayersStats.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainPageController {
+
+    @GetMapping("/")
+    public String viewHomePage() {
+        return "index";
+    }
+
+
+}
