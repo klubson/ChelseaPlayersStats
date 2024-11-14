38f5d9ecf736f91b055ce4531f768beea70d27e9
Mateusz Kluba <mateusz.kluba@kalasoft.pl>
2024-11-14 22:32:18 +0100

    refs: - initial commit - #2 - environment - #3 - database

---
+++ src/main/java/klubson/chelseapoland/ChelseaPlayersStats/ChelseaPlayersStatsApplication.java
@@ -0,0 +1,13 @@
+package klubson.chelseapoland.ChelseaPlayersStats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChelseaPlayersStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChelseaPlayersStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/klubson/chelseapoland/ChelseaPlayersStats/ChelseaPlayersStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package klubson.chelseapoland.ChelseaPlayersStats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChelseaPlayersStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
